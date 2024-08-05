a9b7536f769ec72254d62c6b4bbd47d583424d07
tarekhdeen <tarekhdeen@gmail.com>
2024-08-05 21:59:10 +0300

    Edit 0

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,30 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
-
+import js from '@eslint/js';
+import react from 'eslint-plugin-react';
+import globals from 'globals';
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  js.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2021
+      }
+    },
+    plugins: {
+      react: react,
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
 ];
